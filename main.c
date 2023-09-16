f6a676fc1498598321bea7321fe5bb9220737dd9
Moe <moeali00000@gmail.com>
2023-09-16 06:48:51 -0700

    another fix

--- main.c
+++ main.c
@@ -9,13 +9,14 @@
  */
 int main(void)
 {
-	char *str = "Hello print!", c = 'M';
+	char *str = "Hello print!", c = 'M', *s = "why new line?";
 	int count = 0;
 
 	count = _printf("%s\n", str);
 	printf("%i\n", count);
 	count = printf("%s\n", str);
 	_printf("another test for Hello! %s\n", str);
+	_printf("%s\n", s);
 	printf("%i\n", count);
 	count = _printf("that's my: %c\n", c);
 	printf("that's my: %c\n%i\n", c, count);
