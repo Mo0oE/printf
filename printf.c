9afafe438505e1b5897b85abf5e91cadd233febb
Moe <moeali00000@gmail.com>
2023-09-17 11:24:54 -0700

    another fix

--- printf.c
+++ printf.c
@@ -13,8 +13,8 @@ int _printf(const char *format, ...)
 	char *str;
 	va_list list;
 
-	if (!format)
-		return (-1);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+                 return (-1);
 	va_start(list, format);
 	while (*format)
 	{
