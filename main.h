1b9be4a292a11fa22c00ecdfee2c38ab0249e09a
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-09-19 07:35:27 -0700

    Added Advanced Functions -- Reverse, ROT13

--- main.h
+++ main.h
@@ -15,6 +15,9 @@ int printInt(int n);
 int print_binary(int n);
 void print_number(int n);
 int number_nums(int n);
+int print_reverse(char *str);
+char rot13(char c);
+int printROT13(char *str);
 
 int _printf(const char *format, ...);
 
